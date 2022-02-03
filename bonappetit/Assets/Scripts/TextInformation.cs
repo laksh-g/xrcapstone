e26b720f75f32730a04ccc57af2b558457a666f8
klc715 <klc715@uw.edu>
2022-02-03 10:56:29 -0800

    object info

--- bonappetit/Assets/Scripts/TextInformation.cs
+++ bonappetit/Assets/Scripts/TextInformation.cs
@@ -35,16 +35,51 @@ public class TextInformation : MonoBehaviour
     void UpdateText() 
     {
         text.text = "Item: " + selectedObject.name + '\n';
-        var liquidContainer = selectedObject.GetComponent<LiquidContainer>();
-        if (liquidContainer != null) 
-        {
-            text.text += "Capacity: " + liquidContainer.capacity + '\n';
-            text.text += "Current Volume: " + liquidContainer.currentVolume + '\n';
-        }
+
         var temp = selectedObject.GetComponent<Temperature>();
         if (temp != null) {
-            text.text += "Temperature: " + temp.temp + '\n';
+            text.text += "Current temperature: " + temp.temp.ToString("F1") + " C" + '\n';
         }
+
+        var cook = selectedObject.GetComponent<Cookable>();
+        if (cook != null) {
+            // text.text += "Cook Status: " + cook.??? + '\n';
+            // cookable needs a way for me to get status
+        }
+
+        var steak = selectedObject.GetComponent<Steak>();
+        if (steak != null) {
+            // searTime needs to be public, or have a get method
+            //text.text += "Sear Time: " + steak.searTime.ToString("F0") + " s" + '\n';
+            text.text += "Rest time: " + steak.restTime.ToString("F0") + " s" + '\n';
+            text.text += "Doneness: " + steak.GetDonenessLabel() + '\n';
+            text.text += "Salt: " + steak.salt.ToString("F2") + " g" + '\n';
+            text.text += "Pepper: " + steak.pepper.ToString("F2") + " g" + '\n';
+        }
+
+        var fries = selectedObject.GetComponent<Fries>();
+        if (fries != null) {
+            //text.text += "Salt: " + fries.salt.ToString("F2") + " g" + '\n';
+            //text.text += "Parsley: " + fries.parsley.ToString("F2") + " g" + '\n';
+        }
+
+        var bearnaise = selectedObject.GetComponent<Bearnaise>();
+        if (bearnaise != null) {
+            text.text += "Volume: " + bearnaise.volume.ToString("F2") + " g" + '\n'; // or oz
+            //text.text += "Heating time: " + bearnaise.???? + " s" + '\n';
+        }
+
+        var knob = selectedObject.GetComponent<Knob>();
+        if (knob != null) {
+            text.text += "Setting: " + knob.getLabel() + '\n';
+        }
+
+        var liquidContainer = selectedObject.GetComponent<LiquidContainer>();
+        if (liquidContainer != null) {
+            text.text += "Capacity: " + liquidContainer.capacity.ToString("F0") + " mL" + '\n';
+            text.text += "Current volume: " + liquidContainer.currentVolume.ToString("F0") + " mL" + '\n';
+        }
+
     }
 
 }
