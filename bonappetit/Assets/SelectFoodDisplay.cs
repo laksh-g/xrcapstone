2d4cf56a81fe69ff7f44f1bfcb2f30098ff507a1
Laksh Gupta <lakshgupta2000@gmail.com>
2022-03-08 20:41:58 -0800

    Added new End game scene and fixed lobby menu selection bugs

--- bonappetit/Assets/SelectFoodDisplay.cs
+++ bonappetit/Assets/SelectFoodDisplay.cs
@@ -9,10 +9,23 @@ public class SelectFoodDisplay : XRSimpleInteractable
     private bool selected;
     [SerializeField]
     public int itemId;
+
+    List<Color> startColor;
+    List<Renderer> currRenders;
+
+    //public GameObject currObj;
     // Start is called before the first frame update
     void Start()
     {
+        startColor = new List<Color>();
+        currRenders = new List<Renderer>();
         selected = false;
+        // Renderer[] renders = currObj.GetComponentsInChildren<Renderer>();
+        // foreach (Renderer r in renders)
+        // {
+        //     startColor.Add(r.material.color);
+        //     currRenders.Add(r);
+        // }
     }
 
     override protected void OnSelectEntering(SelectEnterEventArgs interactor)
@@ -24,14 +37,22 @@ public class SelectFoodDisplay : XRSimpleInteractable
             //ht["ticketOption"] = (int) mainSlider.value;
             if(ht.ContainsKey("FoodDisplay")){
                 if(selected){
-                    ht["FoodDisplay"] = ((string)ht["FoodDisplay"]).Remove(itemId, 1).Insert(itemId,"0");
-                }else{
                     ht["FoodDisplay"] = ((string)ht["FoodDisplay"]).Remove(itemId, 1).Insert(itemId,"1");
+                }else{
+                    ht["FoodDisplay"] = ((string)ht["FoodDisplay"]).Remove(itemId, 1).Insert(itemId,"0");
                 }
                 Debug.Log("Updated string to" + ht["FoodDisplay"]);
-                PhotonNetwork.CurrentRoom.SetCustomProperties(ht);   
+                PhotonNetwork.CurrentRoom.SetCustomProperties(ht);
             }
         }
+
+        // if (selected){
+        //     for (int i = 0; i < startColor.Count; i++){
+        //             Color currentColor = Color.Lerp(startColor[i], Color.yellow, Mathf.Cos(elapsed / blinkTime) * 0.7f + 0.3f);
+        //             currRenders[i].material.color = currentColor;
+        //             //currRenders[idx][i].material = highlightMaterial;
+        //         }
+        // }
         base.OnSelectEntering(interactor);
     }
 }
