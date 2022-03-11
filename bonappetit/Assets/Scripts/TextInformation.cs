245b326d353bb471c33331ccff6e0bc78ab13814
Johnathon <jmlg@cs.washington.edu>
2022-03-11 15:57:21 -0800

    Removed debug info

--- bonappetit/Assets/Scripts/TextInformation.cs
+++ bonappetit/Assets/Scripts/TextInformation.cs
@@ -74,13 +74,14 @@ public class TextInformation : MonoBehaviour
     void UpdateText() 
     {
         //text.text += "Name: " + selectedObject.name + '\n';
-
+/* Debug info
         var view = selectedObject.GetComponent<PhotonView>();
         if (view != null) {
             text.text += "Is my item: " + view.IsMine + "\n";
         }
 
         text.text += "Item layer: " + selectedObject.layer + "\n";
+        */
         var desc = selectedObject.GetComponent<Description>();
         string tagStr = selectedObject.tag;
         if (desc == null)
