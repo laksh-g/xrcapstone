04884eb1076181f390f28410ef39e9a2a7e3ea4f
Laksh Gupta <lakshgupta2000@gmail.com>
2022-03-13 14:14:45 -0700

    added kick players out on master disconnect

--- bonappetit/Assets/Scripts/ReturnToLobby.cs
+++ bonappetit/Assets/Scripts/ReturnToLobby.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class ReturnToLobby : MonoBehaviourPunCallbacks
+public class ReturnToLobby : MonoBehaviourPunCallbacks, IInRoomCallbacks
 {
     public UnityEngine.XR.Interaction.Toolkit.InputHelpers.Button inputHelpers = UnityEngine.XR.Interaction.Toolkit.InputHelpers.Button.MenuButton;
     public XRNode controller = XRNode.LeftHand;
@@ -19,5 +20,16 @@ public class ReturnToLobby : MonoBehaviourPunCallbacks
             PhotonNetwork.Disconnect();
             PhotonNetwork.LoadLevel(0);
         }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        ExitGames.Client.Photon.Hashtable ht = PhotonNetwork.CurrentRoom.CustomProperties;
+        if((int)ht["HeadChefRole"] == otherPlayer.ActorNumber){
+            PhotonNetwork.Disconnect();
+            PhotonNetwork.LoadLevel(0);
+            Debug.LogError("Master Client Disconnected");
+        }
+        base.OnPlayerLeftRoom(otherPlayer);
     }
 }
