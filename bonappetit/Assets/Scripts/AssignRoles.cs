0c45ffda3a17c327f8adc3d7960667845cd28f31
Laksh Gupta <lakshgupta2000@gmail.com>
2022-03-13 19:25:09 -0700

    Added RPC Role selection

--- bonappetit/Assets/Scripts/AssignRoles.cs
+++ bonappetit/Assets/Scripts/AssignRoles.cs
@@ -5,6 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class AssignRoles : MonoBehaviourPunCallbacks
 {
@@ -17,14 +18,25 @@ public class AssignRoles : MonoBehaviourPunCallbacks
     public Button RoomSettings;
     private bool buttonActivated;
 
+    private PhotonView _view;
+
+    [SerializeField]
+    public Dictionary<int, int> RoleMap = new Dictionary<int, int>();
+
     // Start is called before the first frame update
     void Start()
     {
         StartGame.interactable = false;
         RoomSettings.interactable = false;
         buttonActivated = false;
+        _view = GetComponent<PhotonView>();
+        RoleMap[0] = -1;
+        RoleMap[1] = -1;
+        RoleMap[2] = -1;
+        RoleMap[3] = -1;
 
-        InvokeRepeating("updateButtons", 0.1f, 0.4f);
+        updateButtons();
+        InvokeRepeating("UpdateLastButton", 0.1f, 5f);
     }
 
     // Update is called once per frame
@@ -38,42 +50,45 @@ public class AssignRoles : MonoBehaviourPunCallbacks
     //     yield return new WaitForSeconds(1.5f);
     // }
 
+    public void UpdateLastButton(){
+        ExitGames.Client.Photon.Hashtable ht = PhotonNetwork.CurrentRoom.CustomProperties;
+        if(!buttonActivated){
+            if((string)ht["difficulty"] == "Fine Dining Kitchen"){
+                SousButton.gameObject.SetActive(true);
+                buttonActivated = true;
+            }else{
+                SousButton.gameObject.SetActive(false);
+            }
+        }
+    }
+
     public void updateButtons(){
         if (PhotonNetwork.CurrentRoom != null) {
-            ExitGames.Client.Photon.Hashtable ht = PhotonNetwork.CurrentRoom.CustomProperties;
-            if(!buttonActivated){
-                if((string)ht["difficulty"] == "Fine Dining Kitchen"){
-                    SousButton.gameObject.SetActive(true);
-                    buttonActivated = true;
-                }else{
-                    SousButton.gameObject.SetActive(false);
-                }
-            }
             
-            if(ht.ContainsKey("RotisseurRole") && (int)ht["RotisseurRole"] != -1){
+            if(RoleMap[0] != -1){
                 RotisseurButton.interactable = false;
-                RotisseurButton.GetComponentInChildren<TMP_Text>().text = ((int)ht["RotisseurRole"]).ToString();
+                RotisseurButton.GetComponentInChildren<TMP_Text>().text = (RoleMap[0]).ToString();
             } else {
                 RotisseurButton.interactable = true;
                 RotisseurButton.GetComponentInChildren<TMP_Text>().text = "Rotisseur";
             }
-            if(ht.ContainsKey("SaucierRole") && (int)ht["SaucierRole"] != -1){
+            if(RoleMap[1] != -1){
                 SaucierButton.interactable = false;
-                SaucierButton.GetComponentInChildren<TMP_Text>().text = ((int)ht["SaucierRole"]).ToString();
+                SaucierButton.GetComponentInChildren<TMP_Text>().text = (RoleMap[1]).ToString();
             } else {
                 SaucierButton.interactable = true;
                 SaucierButton.GetComponentInChildren<TMP_Text>().text = "Saucier";
             }
-            if(ht.ContainsKey("HeadChefRole") && (int)ht["HeadChefRole"] != -1){
+            if(RoleMap[2] != -1){
                 HeadChefButton.interactable = false;
-                HeadChefButton.GetComponentInChildren<TMP_Text>().text = ((int)ht["HeadChefRole"]).ToString();
+                HeadChefButton.GetComponentInChildren<TMP_Text>().text = (RoleMap[2]).ToString();
             } else {
                 HeadChefButton.interactable = true;
                 HeadChefButton.GetComponentInChildren<TMP_Text>().text = "Head Chef";
             }
-            if(ht.ContainsKey("SousChefRole") && (int)ht["SousChefRole"] != -1){
+            if(RoleMap[3] != -1){
                 SousButton.interactable = false;
-                SousButton.GetComponentInChildren<TMP_Text>().text = ((int)ht["SousChefRole"]).ToString();
+                SousButton.GetComponentInChildren<TMP_Text>().text = (RoleMap[3]).ToString();
             } else {
                 SousButton.interactable = true;
                 SousButton.GetComponentInChildren<TMP_Text>().text = "Sous Chef";
@@ -85,22 +100,16 @@ public class AssignRoles : MonoBehaviourPunCallbacks
     {
         Debug.Log("Selected role" + role);
         if (PhotonNetwork.CurrentRoom != null) {
-            ExitGames.Client.Photon.Hashtable ht = PhotonNetwork.CurrentRoom.CustomProperties;
-            ht[role] = PhotonNetwork.LocalPlayer.ActorNumber;
+            //ExitGames.Client.Photon.Hashtable ht = PhotonNetwork.CurrentRoom.CustomProperties;
+            //ht[role] = PhotonNetwork.LocalPlayer.ActorNumber;
         
             string[] roles = {"RotisseurRole", "SaucierRole", "HeadChefRole", "SousChefRole"};
-            
-            foreach(string name in roles)
-            {
-                if(name != role) {
-                    if(ht.ContainsKey(name) && (int) ht[name] == PhotonNetwork.LocalPlayer.ActorNumber) {
-                        Debug.Log(string.Format("Removed role {0} with player actor number {1}.", name, ht[name]));
-                        //ht.Remove(name);
-                        ht[name] = -1;
-                    }
-                }
-            }
-            PhotonNetwork.CurrentRoom.SetCustomProperties(ht);
+
+            int index = Array.IndexOf(roles, role);
+
+
+            _view.RPC("SendRoleUpdates", RpcTarget.AllViaServer, index, PhotonNetwork.LocalPlayer.ActorNumber);
+
 
             ExitGames.Client.Photon.Hashtable playerCustomProps = PhotonNetwork.LocalPlayer.CustomProperties;
             playerCustomProps["role"] = role;
@@ -116,5 +125,43 @@ public class AssignRoles : MonoBehaviourPunCallbacks
                 //StartGame.interactable = true;
             }
         }
+    }
+
+    [PunRPC]
+    public void SendRoleUpdates(int roleID, int playerID) {
+        List<int> updates = new List<int>();
+        foreach(int role in RoleMap.Keys){
+            if(RoleMap[role] == playerID){
+                updates.Add(role);
+                //RoleMap[role] = -1;
+            }
+        }
+
+        foreach(int up in updates){
+            RoleMap[up] = -1;
+        }
+
+        if(RoleMap[roleID] == -1){
+            RoleMap[roleID] = playerID;
+        }
+
+        updateButtons();
+        // if (connected && id == plateID) {
+        //     connected = false;
+        //     point.tag = tag; // reset tag
+        //     point = null;
+        //     //Destroy(_joint);
+        //     //_joint = null;
+        //     gameObject.layer = 9; // set back to food layer
+        //     //_transform.parent = null;
+        //     _rb.isKinematic = false;
+        //     plateID = -1;
+        //     Dish d = PhotonView.Find(id).GetComponent<Dish>();
+        //     if (d != null) {
+        //         d.connectedItems.Remove(_view.ViewID);
+        //         Debug.LogError("Removed view " + _view.ViewID + " from " + d.connectedItems.ToString());
+        //     }
+            
+        // }
     }
 }
